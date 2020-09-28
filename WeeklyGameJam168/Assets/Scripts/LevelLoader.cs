d4517836bf1a4cea8200daf1fda31999390984a6
Jamie <Jamiehyland01@gmail.com>
2020-09-28 20:55:02 +0100

    File for loading the next level

---
+++ WeeklyGameJam168/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public Animator transition;
+    public float transitionTime = 1f;
+    // Update is called once per frame
+   
+
+
+
+
+    public void LoadNextLevel(){
+        Debug.Log("Loading");
+       StartCoroutine(loadLevel(SceneManager.GetActiveScene().buildIndex+1));
+    }
+
+
+    IEnumerator loadLevel(int levelIndex){
+        yield return new WaitForSeconds(2f);
+            // transition.SetTrigger("Play");
+            // yield return new WaitForSeconds(transitionTime);
+            //  transition.SetTrigger("Start");
+            //  yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+
+}
