da60e49fc9aa4c41e396e1e64db5024da49441e3
Robert <robizal@hotmail.com>
2020-09-28 21:39:44 +0100

    Edited spikes script

--- WeeklyGameJam168/Assets/Scripts/SpikedPlatform.cs
+++ WeeklyGameJam168/Assets/Scripts/SpikedPlatform.cs
@@ -7,12 +7,17 @@ public class SpikedPlatform : MonoBehaviour
    
   
   Animator animator;
+  public Animator heroAnimator;
+
+
   [Range(0f,5f)]  public float secondsTillFlipToSpikes = 1f;
   [Range(0,5f)]    public float secondsTillFlipToFloor = 1f;
-  void Awake(){
+  void Awake()
+    {
         animator = this.GetComponent<Animator>();
         StartCoroutine("Rotate");
   }
+
   IEnumerator Rotate(){
         while(true){
              yield return new WaitForSeconds(secondsTillFlipToSpikes);
@@ -23,9 +28,11 @@ public class SpikedPlatform : MonoBehaviour
   }
 
 
-  private void OnTriggerEnter2D(Collider2D other) {
-        if(other.gameObject.name == "Hero"){
-              other.gameObject.GetComponent<MoveController>().registerHit();
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<MoveController>().registerHit();
         }
   }
 }
