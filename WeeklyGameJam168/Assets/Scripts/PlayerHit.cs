ea0e998c24e0f5e1a452fdef49f13c6a1fa10676
Jamie <Jamiehyland01@gmail.com>
2020-09-28 20:55:30 +0100

    added script to register player hit

---
+++ WeeklyGameJam168/Assets/Scripts/PlayerHit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHit : MonoBehaviour
+{
+    Animator animator;
+    public Transform respawnPoint;
+
+    private void Awake() {
+        animator = this.GetComponent<Animator>();    
+    }
+ public void registerHit(){
+        animator.SetTrigger("hitBySpike");
+        Debug.Log("registering hit");
+        animator.SetTrigger("hitBySpike");
+        this.transform.position = respawnPoint.position;
+    }
+}
