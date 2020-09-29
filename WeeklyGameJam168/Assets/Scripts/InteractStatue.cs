5ea44c25e6d41a58b14213c856d4e51c1e620891
Jamie <Jamiehyland01@gmail.com>
2020-09-29 16:24:06 +0100

    scripts

--- WeeklyGameJam168/Assets/Scripts/InteractStatue.cs
+++ WeeklyGameJam168/Assets/Scripts/InteractStatue.cs
@@ -9,6 +9,7 @@ using UnityEngine;
 
 public class InteractStatue : MonoBehaviour
 {
+    public TorchSwitch TorchSwitch;
     private Animator anim; //create animator
     private Boolean playerTouching = false;
     private GameObject torch;
@@ -33,16 +34,18 @@ public class InteractStatue : MonoBehaviour
     {
        if (Input.GetKeyDown(KeyCode.F) && playerTouching == true && torch.transform.parent == hero1.transform) //when f pressed.
         {
-            torch.transform.position = statue.transform.position + new Vector3(-.6f, .8f, 0);
-            torch.transform.parent = statue.transform;
+            // torch.transform.position = statue.transform.position + new Vector3(-.6f, .8f, 0);
+            // torch.transform.parent = statue.transform;
+           // TorchSwitch.SwitchTorchParent(hero1,this.gameObject);
             anim.SetTrigger("Rotating"); //activates rotating trigger in animator
             GetComponent<ScreenFadeManager>().FlipScreens();
         }
 
        if (Input.GetKeyDown(KeyCode.F) && playerTouching == true && torch.transform.parent == hero2.transform)
         {
-            torch.transform.position = statue.transform.position + new Vector3(-.6f, .8f, 0);
-            torch.transform.parent = statue.transform;
+            // torch.transform.position = statue.transform.position + new Vector3(-.6f, .8f, 0);
+            // torch.transform.parent = statue.transform;
+        //    TorchSwitch.SwitchTorchParent(hero2,this.gameObject);
             anim.SetTrigger("Rotating"); //activates rotating trigger in animator
             GetComponent<ScreenFadeManager>().FlipScreens();
         }
---
+++ WeeklyGameJam168/Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public bool besideTorch = false;
+    public bool besideTikiTorch = false;
+    public bool besideStatue = false;
+
+   private void OnTriggerEnter2D(Collider2D other) {
+       if(other.gameObject.name == "Torch")besideTorch = true;
+       if(other.gameObject.name == "TikiTorch")besideTikiTorch = true;
+       if(other.gameObject.name == "Statue")besideStatue = true;
+   }
+   private void OnTriggerExit2D(Collider2D other) {
+       if(other.gameObject.name == "Torch")besideTorch = false;
+       if(other.gameObject.name == "TikiTorch")besideTikiTorch = false;
+       if(other.gameObject.name == "Statue")besideStatue = false;
+   }
+}
