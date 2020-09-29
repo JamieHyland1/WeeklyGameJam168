5f78888433bd203131862956d87f14059249e328
Robert <robizal@hotmail.com>
2020-09-29 13:14:27 +0100

    update pickup script

--- WeeklyGameJam168/Assets/Scripts/PickUp.cs
+++ WeeklyGameJam168/Assets/Scripts/PickUp.cs
@@ -5,33 +5,50 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
+    private GameObject hero1;
+    private GameObject hero2;
     private Boolean playerTouching = false;
-    private GameObject hero;
 
     void Start()
     {
-        hero = GameObject.Find("Hero");
+        hero1 = GameObject.Find("Hero");
+        hero2 = GameObject.Find("Hero 2");
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F) && playerTouching == true) //when f pressed.
+        if (Input.GetKeyDown(KeyCode.F) && playerTouching == true && this.transform.parent != hero1.transform) //when f pressed.
         {
-            Debug.Log("Pickup");
-            this.transform.position = hero.transform.position + new Vector3(1f, 1f, 0f);
-            this.transform.parent = hero.transform;
+            Debug.Log("Pickup1");
+            this.transform.position = hero1.transform.position + new Vector3(.2f, .2f, 0f);
+            this.transform.parent = null;
+            this.transform.parent = hero1.transform;
+        }
+ 
+        if (Input.GetKeyDown(KeyCode.F) && playerTouching == true && hero2.GetComponent<MoveController>().enabled == true && this.transform.parent != hero2.transform) //when f pressed.
+        {
+            Debug.Log("Pickup2" + this.transform.parent);
+            this.transform.position = hero2.transform.position + new Vector3(.2f, .2f, 0f);
+            this.transform.parent = null;
+            this.transform.parent = hero2.transform;
         }
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        playerTouching = true;
-        Debug.Log("Enter");
+        if (other.tag == "Player")
+        {
+            playerTouching = true;
+            Debug.Log("Enter");
+        }
     }
 
-    void OnTriggerExit(Collider other)
+    void OnTriggerExit2D(Collider2D other)
     {
-        playerTouching = false;
-        Debug.Log("Exit");
+        if (other.tag == "Player")
+        {
+            playerTouching = false;
+            Debug.Log("Exit");
+        }
     }
 }
