06cad043094c46e72f441207ad457f3c0f2f5467
Robert <robizal@hotmail.com>
2020-10-01 12:48:11 +0100

    modified and deleted scripts

--- WeeklyGameJam168/Assets/Scripts/TorchSwitch.cs
+++ WeeklyGameJam168/Assets/Scripts/TorchSwitch.cs
@@ -6,20 +6,27 @@ public class TorchSwitch : MonoBehaviour
 {
     public GameObject torch;
 
-    public Transform getTorchParent(){
+    public Transform getTorchParent()
+    {
         return torch.transform.parent;
     }
 
-    public void torchPickup(GameObject g){
+    public void torchPickup(GameObject g)
+    {
        torch.transform.position =  g.transform.position + new Vector3(.2f, .2f, 0f);
-       torch.transform.parent = g.transform;
+        torch.transform.SetParent(null);
+        torch.transform.SetParent(g.transform);
     }
-   public void SwitchTorchParent(GameObject one, GameObject two, Vector3 torchOffset){
-       if(torch.transform.parent == one.transform){
-           torch.transform.position = two.transform.position + torchOffset;
-           torch.transform.parent = null;
-           torch.transform.parent = two.transform;
-       }else if(torch.transform.parent == two.transform){
+   public void SwitchTorchParent(GameObject one, GameObject two, Vector3 torchOffset)
+    {
+       if(torch.transform.parent == one.transform)
+        {
+            torch.transform.position = two.transform.position + torchOffset;
+            torch.transform.parent = null;
+            torch.transform.parent = two.transform;
+        }
+        else if(torch.transform.parent == two.transform)
+        {
            torch.transform.position = one.transform.position + torchOffset;
            torch.transform.parent = null;
            torch.transform.parent = one.transform;
