8aaaf1ae614421c2c90771d78258cf74b69d6268
Jamie <Jamiehyland01@gmail.com>
2020-10-01 15:41:06 +0100

    moved folders and added level music to scenes

---
+++ WeeklyGameJam168/Assets/Scripts/startGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startGame : MonoBehaviour
+{
+    
+    void Update()
+    {
+          //Detect when the Return key is pressed down
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            this.GetComponent<LevelLoader>().LoadNextLevel();
+        }
+    }
+}
